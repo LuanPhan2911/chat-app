a62f89eba9073fb337a295ae0539a5af31319955
Luan@YokKok <97419663+LuanPhan2911@users.noreply.github.com>
2024-04-18 21:39:01 +0700

    fixed

--- pages/api/pusher/auth.ts
+++ pages/api/pusher/auth.ts
@@ -1,5 +1,5 @@
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { pusherServer } from "@/app/lib/pusher";
+import { authOptions } from "@/app/utils/authOptions";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getServerSession } from "next-auth";
 
